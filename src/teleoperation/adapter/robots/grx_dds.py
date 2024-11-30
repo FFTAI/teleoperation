d1e7e37de274f1cc40bdc5243a1947c72eb5e0c9
Yuxiang Gao <yuxiang.gao@fftai.com>
2024-11-30 14:47:26 +0800

    🎨 refactor dds integration
    Signed-off-by: Yuxiang Gao <yuxiang.gao@fftai.com>

--- src/teleoperation/adapter/robots/grx_dds.py
+++ src/teleoperation/adapter/robots/grx_dds.py
@@ -1,30 +1,60 @@
 import logging
+import tempfile
 import time
 from pathlib import Path
 
-import numpy as np
-from fourier_grx_dds.gravity_compensation import GravityCompensator, Upsampler
-from fourier_grx_dds.utils import ControlMode, GR1ControlGroup
+from fourier_grx_dds.gravity_compensation import GravityCompensator
+from fourier_grx_dds.utils import GR1ControlGroup
+from omegaconf import DictConfig, OmegaConf
 
 logger = logging.getLogger(__name__)
+
+
+def init_encoders(self):
+    import os
+
+    """Initialize the encoders state."""
+    encoders_state, integrality = self.connector.get_encoders_state()
+    assert integrality, f"Error: Can not fetch the whole encoders_state."
+    assert os.path.exists(self.encoders_state_path), f"Encoders state file[{self.encoders_state_path}] not founded."
+    logger.info(f"Load encoders state from {self.encoders_state_path}")
+    self.encoders_state = OmegaConf.load(self.encoders_state_path)
+    if integrality:
+        for name in encoders_state:
+            angle = encoders_state[name].angle
+            self.encoders_state[name]["poweron_pose"] = angle
+            self.encoders_state[name]["calibration_pose"] = angle
+        OmegaConf.save(self.encoders_state, self.encoders_state_path)
+        logger.info(f"Encoders poweron state saved to {self.encoders_state_path}")
+    logger.info("Encoders initialized.")
+
+
+GravityCompensator.init_encoders = init_encoders
 
 
 class GR1Robot_DDS:
     def __init__(
         self,
-        dds_cfg_path: Path,
+        dds_cfg: DictConfig,
         controlled_joint_indices: list,
         default_qpos: list,
         named_links: dict,
+        calibrate_encoders: bool = False,
     ):
-        self.client = GravityCompensator(dds_cfg_path, target_hz=120)
         self.controlled_joint_indices = controlled_joint_indices
         self.default_qpos = default_qpos
         self.named_links = named_links
 
-        logger.info(f"Initializing {self.__class__.__name__}...")
-        logger.info(f"cfg_path: {dds_cfg_path}")
-        logger.info(f"Config: {self.default_qpos}")
+        # write dds_cfg to tempfile
+        config_str = OmegaConf.to_yaml(dds_cfg, resolve=True)
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".yaml", mode="w") as temp_file:
+            temp_file.write(config_str)
+
+            self.client = GravityCompensator(Path(temp_file.name), target_hz=120)
+
+            logger.info(f"Initializing {self.__class__.__name__}...")
+            logger.info(f"cfg_path: {temp_file.name}")
+            logger.info(f"Config: {self.default_qpos}")
 
     @property
     def joint_positions(self):
@@ -37,26 +67,7 @@ class GR1Robot_DDS:
         time.sleep(1.0)
         self.client.enable()
         time.sleep(1.0)
-        left_arm_position = [
-            -0.10834163755741072,
-            -0.07329939774949822,
-            0.06528929994794762,
-            -1.4866168673727456,
-            -0.15687147335078633,
-            -0.13071683482883256,
-            -0.17893611111972085,
-        ]
-        right_arm_position = [
-            0.10834163755741072,
-            -0.07329939774949822,
-            0.06528929994794762,
-            -1.4866168673727456,
-            -0.15687147335078633,
-            -0.13071683482883256,
-            0.17893611111972085,
-        ]
-        arm_position = left_arm_position + right_arm_position
-        self.client.move_joints(GR1ControlGroup.UPPER, arm_position, duration=2.0)
+        self.client.move_joints(GR1ControlGroup.UPPER_EXTENDED, self.default_qpos, duration=2.0)
         logger.info(f"Connected to {self.__class__.__name__}.")
 
     def command_joints(self, positions, gravity_compensation=False):
@@ -64,10 +75,6 @@ class GR1Robot_DDS:
 
     # For safety use of interpolation move to the initial position
     def init_command_joints(self, positions):
-        print("###################################################")
-        print("Prepare to move the robot to the current position, please be safe")
-        input("init_control_joints(press enter):")
-        print("###################################################")
         self.client.move_joints(GR1ControlGroup.ALL, positions, duration=1.0)
 
     def stop_joints(self):
